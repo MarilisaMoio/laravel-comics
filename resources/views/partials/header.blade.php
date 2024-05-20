4f07cb860a10c745072b204e1d40fbc6469c964b
Marilisa Moio <moiomarilisa@gmail.com>
2024-05-20 18:04:01 +0200

    spooky scary skeleton

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,12 @@
+<header>
+    <div class="container">
+        <img src="../assets/img/dc-logo.png" alt="">
+        <nav>
+            <ul>
+                <li>ciao</li>
+            </ul>
+        </nav>
+    </div>
+</header>
+
+{{-- <li v-for="element, index in navElements" :class="{ 'active': index == activeElement }" @click="changeActive(index)"><a href="#">{{ element }}</a></li> --}}
